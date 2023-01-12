b033e9ee9e24fd0c355c02a728155c7e6d107b83
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-13 02:49:35 +0530

    Solved ;)

--- Merge_Two_Sorted_Lists.py
+++ Merge_Two_Sorted_Lists.py
@@ -25,5 +25,26 @@ The number of nodes in both lists is in the range [0, 50].
 Both list1 and list2 are sorted in non-decreasing order.
 '''
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
+def MergeLinkedList(list1,list2):
+    dummy = ListNode()
+    tail = dummy
 
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+
+        tail = tail.next
+
+    tail.next = list1 or list2
+
+    return dummy.next
