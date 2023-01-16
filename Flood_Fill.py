e27ba4b7fa7298840a67e53645184b464700cc78
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-16 19:50:40 +0530

    Solved ;)

--- Flood_Fill.py
+++ Flood_Fill.py
@@ -31,3 +31,18 @@ n == image[i].length
 
 '''
 
+def FloodFill(image,sr,sc,color):
+    OriginalColor = image[sr][sc]
+    height = len(image)
+    width = len(image[0])
+
+    def dfs(sr,sc):
+        if 0<=sr<height and 0<=sc<width and image[sr][sc] == OriginalColor and image[sr][sc] != color:
+            image[sr][sc] = color
+            dfs(sr+1,sc)
+            dfs(sr-1,sc)
+            dfs(sr,sc+1)
+            dfs(sr,sc-1)
+
+    dfs(sr,sc)
+    return image
