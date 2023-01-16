1e8f5b783fe20f10768bd32276c2fb5920e0c3c3
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-16 15:50:14 +0530

    Solved ;)

--- Valid_Anagram.py
+++ Valid_Anagram.py
@@ -19,8 +19,32 @@ Constraints:
 1 <= s.length, t.length <= 5 * 104
 s and t consist of lowercase English letters.
  
- 
+
 Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 
 '''
 
+def ValidAnagram(str1,str2):
+    if len(str1) != len(str2):
+        return False
+
+    sMap,tMap = {},{}
+    for c in str1:
+        if c in sMap:
+            sMap[c] += 1
+        else:
+            sMap = 1
+
+    for c in str2:
+        if c in tMap:
+            tMap[c] += 1
+        else:
+            tMap = 1
+        
+    for char in sMap:
+        if sMap[char] != tMap.get(char,0):
+            return False
+    
+    return True
+
+    
