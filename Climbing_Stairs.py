3352550ea7b9ee9047d97741fefc5495b652383e
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-17 22:17:38 +0530

    Solved ;)

--- Climbing_Stairs.py
+++ Climbing_Stairs.py
@@ -26,3 +26,10 @@ Constraints:
 1 <= n <= 45
 
 '''
+def climbStairs(n):
+    one,two = 1,1
+    for i in range(n-1):
+        temp = one
+        one = one + two
+        two = temp
+    return one
