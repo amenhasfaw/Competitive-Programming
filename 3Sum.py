3db85201853e5c1fe218b096d7dbeebdb9818562
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-23 23:44:29 +0530

    Solved ;)

--- 3Sum.py
+++ 3Sum.py
@@ -31,5 +31,24 @@ Constraints:
 
 '''
 
-def threeSum(nums):
-    pass
+def ThreeSum(nums):
+    res = []
+    nums.sort()
+
+    for i,a in enumerate(nums):
+        if i > 0 and nums[i-1] == a:
+            continue
+
+        l,r = i + 1, len(nums) - 1
+        while l<r:
+            threesum = a + nums[l] + nums[r]
+            if threesum > 0:
+                r -= 1
+            elif threesum < 0:
+                l += 1
+            else:
+                res.append([a,nums[l],nums[r]])
+                l += 1
+                while l < r and nums[l] == nums[l-1]:
+                    l+=1
+    return res
