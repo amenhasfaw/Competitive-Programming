d298926f795fb50686cb28e50e717cbae8d30937
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-23 19:12:30 +0530

    Solved ;)

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -25,3 +25,14 @@ s consists of English letters, digits, symbols and spaces.
 
 '''
 
+def lengthofLongestSubstring(s:str):
+    charSet = set()
+    l = 0
+    maxLength = 0
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        maxLength = max(maxLength,len(charSet))
+    return maxLength
