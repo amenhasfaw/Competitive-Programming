4d3216f15f2f52b776d59e9486d93f1f3098f9b2
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-23 18:20:05 +0530

    Solved ;)

--- K_Closest_Point_to_Origin.py
+++ K_Closest_Point_to_Origin.py
@@ -25,5 +25,20 @@ Constraints:
 
 '''
 
+import heapq
+
+
 def KClosest(points,k):
-    pass
+    minHeap = []
+    for x,y in points:
+        dist = (x ** 2) + (y ** 2)
+        minHeap.append([dist,x,y])
+
+    heapq.heapify(minHeap)
+    res = []
+    while k > 0:
+        dist, x, y = heapq.heappop(minHeap)
+        res.append([x,y])
+        k -= 1
+
+    return res
