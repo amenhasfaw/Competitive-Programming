c5946c856e5101035e292f1117e623b888066b68
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-29 23:57:34 +0530

    Solved ;)

--- Evaluate_Reverse_Polish_Notation.py
+++ Evaluate_Reverse_Polish_Notation.py
@@ -41,4 +41,18 @@ tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the ran
 '''
 
 def evalRPN(tokens):
-    pass
+    stack = []
+    for c in tokens:
+        if c == "+":
+            stack.append(stack.pop() + stack.pop())
+        elif c == "*":
+            stack.append(stack.pop() * stack.pop())
+        elif c == "-":
+            a,b = stack.pop(),stack.pop()
+            stack.append(b-a)
+        elif c == "/":
+            a,b = stack.pop(),stack.pop()
+            stack.append(int(b/a))
+        else:
+            stack.append(int(c))
+    return stack[0]
