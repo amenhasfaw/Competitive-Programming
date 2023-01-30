f8316d221502f8744358c937272ac958e349da15
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-01-30 19:37:13 +0530

    Solved ;)

--- Course_Schedule.py
+++ Course_Schedule.py
@@ -5,7 +5,6 @@ Course Schedule
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 Return true if you can finish all courses. Otherwise, return false.
-
  
 
 Example 1:
@@ -31,4 +30,25 @@ All the pairs prerequisites[i] are unique.
 '''
 
 def canfinish(numCourses,prerequisities):
-    pass
+    preMap = {i:[] for i in range(numCourses)}
+    for crs , pre in prerequisites:
+        preMap[crs].append(pre)
+        
+        visited = set()
+    def dfs(crs):
+        if crs in visited:
+            return False
+        if preMap[crs] == []:
+            return True
+
+        visited.add(crs)
+        for pre in preMap[crs]:
+            if not dfs(pre): return False
+        visited.remove(crs)
+        preMap[crs] = []
+        return True
+        
+    for crs in range(numCourses):
+        if not dfs(crs): return False
+    return True
+
