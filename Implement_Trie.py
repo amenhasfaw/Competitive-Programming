0cc52daa8799f784d998ad275a45fa954479fde7
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-06-30 07:50:27 +0300

    Solved ;)

--- Implement_Trie.py
+++ Implement_Trie.py
@@ -32,16 +32,40 @@ word and prefix consist only of lowercase English letters.
 At most 3 * 104 calls in total will be made to insert, search, and startsWith.
 '''
 
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = {}
+        self.isEnd = False
+
 class Trie:
 
     def __init__(self):
-        pass
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        pass
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr =  curr.children[c]
+        curr.isEnd = True
+
 
     def search(self, word: str) -> bool:
-        pass
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.isEnd
 
     def startsWith(self, prefix: str) -> bool:
-        pass
+        curr = self.root
+
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.childre[c]
+        return True
