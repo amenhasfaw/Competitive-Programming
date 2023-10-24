ea4cb96e0d9742916c23ef25895109781a1a7052
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-10-24 08:00:12 +0530

    Solved ;)

--- Numbe of Islands.py
+++ Numbe of Islands.py
@@ -31,5 +31,34 @@ grid[i][j] is '0' or '1'.
 '''
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        pass
+    def numIslands(self, grid) -> int:
+        if not grid:
+            return 0
+        
+        island = 0
+        visit = set()
+        rows, cols = len(grid), len(grid[0])
+
+        def bfs(r,c):
+            q = collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    r,c = row + dr , col + dc
+                    if(r in range(rows) and
+                       c in range(cols) and
+                       grid[r][c] == "1" and
+                        (r,c) not in visit):
+                        q.append((r,c))
+                        visit.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    bfs(r,c)
+                    island += 1
+        
+        return island
